745c889290976ad0f7aa6c962d82342c3de5f597
certainlyMohneeesh <certainlymohneesh@gmail.com>
2025-06-25 18:28:47 +0530

    let's see the change in colour

--- src/components/ContricReaction.tsx
+++ src/components/ContricReaction.tsx
@@ -49,7 +49,7 @@ interface ContricReactionProps {
 const ContricReaction: React.FC<ContricReactionProps> = ({
   circleCount = 5,
   maxSize = 100,
-  color = 'rgba(255, 255, 255, 0)',
+  color = 'rgba(255, 255, 255, 1)',
   rotationSpeed = 1,
   followPointer = true,
   followDelay = 100,
